ef19459a895f6c72b7f88bfec87b0fa8fa7414a2
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-11-02 09:50:34 +0200

    project skeleton created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SupremeLaserArcticMoose::Application.config.secret_token = '9f5b01a59cdcb2dd5c894219577e323bca82fc41865008a3cf523b43836a679419a288988ac0745aabf9f360db3e1bf91434a0a65d676e152938e9d44e07112d'
