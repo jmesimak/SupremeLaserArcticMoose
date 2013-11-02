5df5496079bf30fa5f0997afe1eafb539fdce9a6
Jerry Mesimaki <jerry.mesimaki@cs.helsinki.fi>
2013-11-02 14:58:52 +0200

    superpush

---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,22 @@
+var joinData = [];
+
+$(document).ready(function() {
+    $(".fs_panel").hide();
+    $(".fs_panel").first().show();
+
+    $(".panel_button").bind("click", function() {
+        joinData.push($(this).attr("value"));
+        console.log(joinData);
+        $(this).parent().hide();
+        $(this).parent().next().show();
+
+        if (joinData.length >= 3) {
+            $("#dayslot").val(joinData[0]);
+            $("#dayhalf").val(joinData[1]);
+            $("#timeslot").val(joinData[2]);
+
+            $("#new_membership").submit();
+        }
+    });
+});
+
